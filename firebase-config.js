28a2923ed232a28d5323fc7a431d0a3ab30c6d9c
ACww1116 <mikeyiscool05@gmail.com>
2025-05-18 18:22:24 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -6,6 +6,3 @@ const firebaseConfig = {
   messagingSenderId: "244136822379",
   appId: "1:244136822379:web:daa0dbc6efffae951e932e"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
